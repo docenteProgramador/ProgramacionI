bc75f53d5e6734c58e6b1e61579b7dd3227f859d
Oscar Andrade <oscar.andrade@unitec.edu>
2026-06-22 18:48:32 -0600

    Repaso

---
+++ Q2_26/ExamenDos/src/examendos/ExamenDos.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examendos;
+
+/**
+ *
+ * @author oscar
+ */
+public class ExamenDos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] estudiantes = new int[]{5,10,29,60,50,100};
+        
+        //Cual es la tamanio del arreglo llamado estudiante? 6
+        //Cual es el valor del elemento que estan en la pos 3? 29
+        //Cual es el indice del valor 29 en el arreglo? 2
+        
+        char[][] letras = new char[][]{
+            {'1','5','a','t'},
+            {'g','2','w','3'},
+            {'0','c','1','d'}
+        };
+        
+        //Cuantas columnas y cuantas filas tiene el arreglo bidimensional
+        //llamado letras?  Fil 3, Col 4
+        //_cual es el valor del elemento letras[1][1]?
+        
+        //Si se ingresa 'p' usando la siguiente linea
+        
+        
+        for (int i = 0; i < 1; i++) {
+            //letras[2][i] = scan.next();
+            //Porque tira error?
+            /*
+            Selecion la mejor descripcion del problema
+            a.No tira problema
+            b.No se completa el arreglo
+            c.Mal declarado el for en su condicion
+            */
+        }
+        
+        
+    }
+    
+}
