32dc83c36fee111a22deb26ab5c5ad9e217a0f5b
Oscar Andrade <oscar.andrade@unitec.edu>
2026-08-13 07:59:11 -0600

    Resolucion Ejercicios

---
+++ Monitoreo/src/monitoreo/Monitoreo.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package monitoreo;
+
+/**
+ *
+ * @author oscar
+ */
+public class Monitoreo {
+
+    /**
+     * Desarrolla un programa en Java que simule la lectura de sensores de 
+     * temperatura en un cuarto de servidores. El programa debe solicitar 
+     * el nombre del operador encargado y cuántas lecturas de sensores se 
+     * van a simular. Debe validar que la cantidad de lecturas sea mayor a 0 
+     * y menor o igual a 50. Por cada lectura, el sistema generará una temperatura 
+     * aleatoria en grados Celsius. El programa debe convertir cada lectura a 
+     * Fahrenheit, determinar si hay una alerta de sobrecalentamiento y, al final, 
+     * mostrar un resumen estadístico.
+     */
+    /*
+        1. Comprender el Problema
+            a. ¿Que datos de entrada necesito?
+            b. Transformaciones / Calculos Necesarios
+            c. ¿Que resultados requiero? Que se va a mostrar?
+    
+        2. Diseñar una Posible Solucion
+            a. Diagrama de Flujo
+            b. Pseudocodigo
+        
+        3. Traducir de Manera Incremental el Desarrollo
+            a. Listar las Tareas 
+                i. Declaracion de Variables
+                ii. Tomas de Decision
+                iii. Ciclos de Repeticion
+                iv. Formulas
+            b. Probando que Funcione
+            c. Validaciones
+        
+        4. Pruebas de Escritorio
+            a. Casos
+            b. Escenarios
+            c. Patrones
+    
+        5. Quality Assurance (Testeo)
+    */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }//Fin de Main
+    
+}//Fin de Class
