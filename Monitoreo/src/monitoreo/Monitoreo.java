8f68d4c082764b6555fd78c5f39848abf7c8353c
Oscar Andrade <oscar.andrade@unitec.edu>
2026-08-14 19:27:46 -0600

    Ejerciciod e Practica

--- Monitoreo/src/monitoreo/Monitoreo.java
+++ Monitoreo/src/monitoreo/Monitoreo.java
@@ -4,6 +4,9 @@
  */
 package monitoreo;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author oscar
@@ -46,9 +49,67 @@ public class Monitoreo {
     
         5. Quality Assurance (Testeo)
     */
+    
+    /*
+        Entrada
+    1. Captura de cuantas Lecturas/Mediciones (SCANNER)
+        a. Validar que se capturo
+        
+    2. Registrar de Temperatura (RANDOM)
+        a. Validar que se registro
+        b. Se requieren varias temperaturas (WHILE)
+    
+    3. Captura de nombre del operador (SCANNER)
+        a. Validar que se capturo
+    
+        Transformaciones/Calculos
+    1. Validacion de Lecturas > 0 y <=50 (DO...WHILE)
+    2. Convertir Celsius a Farenheit -> F = (C * 9/5) + 32
+    3. Limite de Sobrecalentamiento (SELECCION DOBLE...if/else)
+    4. Calcular el Resumen Estadistico 
+    
+        Salida
+    1. Promedio, Temp Max y Temp Min, Moda
+    2. Mostrar alerta
+    */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        Random rd = new Random();
         
+        // Declaracion de Variables
+        int lecturas = 0;
+        int temp = 0;
+        int tempF = 0;
+        int numMedicion = 0;
+        String operador = "John Doe";
+        
+        System.out.print("Nombre del Operador: ");
+        operador = input.next().toUpperCase();
+        //System.out.printf("Validacion: %s",operador);
+        System.out.println("");
+        
+        //System.out.printf("Validacion: %d",lecturas);
+        do{
+            System.out.print("Numero de Lecturas:");
+            lecturas = input.nextInt();
+            //if(lecturas<0 || lecturas >50){
+            if(!(lecturas>0 && lecturas <=50)){
+                System.out.println("Valor NO Valido");
+                System.out.println("Tiene que estar entre 0 y 50");
+            }
+        //}while(lecturas<0 || lecturas >50);
+        }while(!(lecturas>0 && lecturas <=50));
+        
+        System.out.println("");
+        while(numMedicion<lecturas){
+            temp = rd.nextInt(1000, 3000);
+            System.out.printf("Num Med: %d \tTemp C: %d\n",numMedicion+1, temp);
+            
+            tempF = (temp * 9/5)+32;
+            System.out.printf("Temp en Farenheit: %d\n",tempF);
+            numMedicion++;
+        }//Fin de While
         
     }//Fin de Main
     
