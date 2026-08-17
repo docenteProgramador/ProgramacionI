c5a0cac8ea96ce5ce003510d6cb4672b1323c067
Oscar Andrade <oscar.andrade@unitec.edu>
2026-08-17 17:59:15 -0600

    Resolucion de Problemas. ESTE CODIGO TIENE ERRORES INTENCIONALES

---
+++ MisionEspacial/src/misionespacial/MisionEspacial.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package misionespacial;
+
+import java.util.Scanner;
+
+
+/*
+    1. Entrada de datos: Solicitar al usuario que ingrese el nombre del piloto de la nave.
+
+    2. Cuenta regresiva: Antes de iniciar la misión, el programa debe imprimir una cuenta 
+        regresiva del 3 al 1 simulando el despegue.
+
+    3. Cálculos matemáticos: La nave tiene un tanque de combustible inicial que se calcula 
+        multiplicando la raíz cuadrada de 10000 por 5 (usando los métodos de la clase Math).
+
+    4. Menú de destinos (do-while y switch): Mostrar un menú cíclico para elegir el planeta destino. 
+        El menú debe repetirse si el usuario ingresa una opción inválida.
+
+        Opción 1: Marte (Distancia = 500)
+        Opción 2: Júpiter (Distancia = 1200)
+        Opción 3: Saturno (Distancia = 2000)
+
+    5. Viaje espacial: Una vez en el espacio, la nave debe avanzar cíclicamente mientras la 
+        distancia al planeta sea mayor a 0 y el combustible sea mayor a 0.
+
+        * En cada iteración (turno) del viaje, la nave avanza 50 unidades de distancia por defecto.
+        * En cada turno, se debe generar un "evento espacial" aleatorio usando la clase Random 
+            (un número entre 1 y 2).
+
+        * Si el evento es 1, la nave choca con un asteroide y pierde 50 unidades de combustible.
+        * Si el evento es 2, la nave encuentra un viento solar a favor y la distancia se reduce 100 unidades extra.
+
+    6. Resultados finales: Al finalizar el bucle del viaje, determinar si la misión fue un éxito 
+        (la distancia llegó a 0 o menos) o un fracaso (se acabó el combustible antes de llegar).
+    
+*/
+
+public class MisionEspacial {
+    public static void main(String[] args) {
+        scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        string piloto;
+        boolean enVuelo = True;
+        double combustible = Math.sqrt(10000) * 5;
+        int opcion = 0
+        int distancia = 0;
+
+        
+        System.out.println("Ingrese el nombre del piloto:");
+        piloto = sc.next();
+
+        System.out.println("Iniciando secuencia de despegue...");
+        for (int i = 1; i < 3; i++) {
+            System.out.println(i);
+        }//Fin de For
+
+        
+        do {
+            System.out.println("Seleccione destino:");
+            System.out.println("1. Marte\n2. Júpiter\n3. Saturno");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    distancia = 500;
+                case 2:
+                    distancia = 1200;
+                case 3:
+                    distancia = 2000;
+                    break;
+                default:
+                    System.out.println("Opción inválida")
+            }//Fin de Switch
+        } while (opcion < 1 || opcion > 3)
+
+        
+
+        while (distancia > 0 || combustible > 0) {
+            System.out.println("Distancia restante: " + distancia + " | Combustible: " + combustible);
+            
+            int evento = rand.NextInt(2) + 1;
+
+            if (evento = 1) {
+                System.out.println("¡Impacto de asteroide!");
+                combustible = combustible + 50;
+            } else if (evento == 2) {
+                System.out.println("¡Viento solar a favor!");
+                distancia += 100;
+            }
+
+            distancia = distancia + 50;
+        }//Fin de While
+
+        if (distancia == 0) {
+            System.out.println("¡Misión completada con éxito, " + piloto + "!");
+        }//Fin de If 
+        else {
+            System.out.println("Misión fallida. Te quedaste sin combustible.");
+        }//Fin de Else
+    }//Fin de Main
+}//Fin de Class
